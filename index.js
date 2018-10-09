1d03d2e54dcde22f039b09b30c1d06b76d0027e6
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-09 00:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidays(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, name){
-  return `It's `
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
 }
